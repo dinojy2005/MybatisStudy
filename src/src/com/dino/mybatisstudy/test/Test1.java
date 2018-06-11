b5d654b3deb525888ecf989fda20e3a526463945
liuxs <liuxs@szkingdom.com>
2018-06-11 14:20:33 +0800

    Upload Mybatis

--- src/src/com/dino/mybatisstudy/test/Test1.java
+++ src/src/com/dino/mybatisstudy/test/Test1.java
@@ -17,6 +17,7 @@ public class Test1 {
 
     public static void main(String[] args) throws IOException
     {
+        System.out.println("Hello, World!");
         Test1 t1  = new Test1();
         // 单行数据
         t1.testE1();
@@ -42,10 +43,12 @@ public class Test1 {
         SqlSession session = sessionFactory.openSession();
         /**
          * 映射sql的标识字符串，
-         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * com.dino.mybatisstudy.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
          * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
          */
-        String statement = "me.gacl.mapping.userMapper.getUser";//映射sql的标识字符串
+        // String statement = "com.dino.mybatisstudy.mapping.userMapper.getUser";//映射sql的标识字符串
+        // com.dino.mybatisstudy.mapping.userMapper.getUser
+        String statement = "com.dino.mybatisstudy.mapping.userMapper.getUser";//映射sql的标识字符串
         //执行查询返回一个唯一user对象的sql
         User user = session.selectOne(statement, 1);
         System.out.println(user);
@@ -59,7 +62,8 @@ public class Test1 {
         InputStream is = Test1.class.getClassLoader().getResourceAsStream(resource);
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
         SqlSession session = sessionFactory.openSession();
-        String statement = "me.gacl.mapping.userMapper.getAllUser";//映射sql的标识字符串
+        // 
+        String statement = "com.dino.mybatisstudy.mapping.userMapper.getAllUser";//映射sql的标识字符串
         List<User> userList = session.selectList(statement);
         Iterator<User> it = userList.iterator();
         while (it.hasNext())
