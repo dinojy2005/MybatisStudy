bdc231ba6e74a25c57da9c4e3bd81cc5f1eccdf5
liuxs <liuxs@szkingdom.com>
2018-06-18 23:11:00 +0800

    Upload JSON

--- src/src/com/dino/mybatisstudy/MybatisProcess/Test1.java
+++ src/src/com/dino/mybatisstudy/MybatisProcess/Test1.java
@@ -1,11 +1,10 @@
-package com.dino.mybatisstudy.test;
+package com.dino.mybatisstudy.MybatisProcess;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
+
 import com.dino.mybatisstudy.domain.User;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -13,18 +12,18 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.util.Iterator;
 import java.util.List;
 
-public class Test1 {
-
+class MybatisProcess
+{
     public static void main(String[] args) throws IOException
     {
         System.out.println("Hello, World!");
-        Test1 t1  = new Test1();
+        MybatisProcess t1  = new MybatisProcess();
         // 单行数据
         t1.testE1();
         // 多行数据
         t1.testE2();
         // 增加数据
-        t1.testE3();
+        t1.testDualUser();
     }
 
     private void testE1()
@@ -32,7 +31,7 @@ public class Test1 {
         //mybatis的配置文件
         String resource = "conf.xml";
         //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        InputStream is = Test1.class.getClassLoader().getResourceAsStream(resource);
+        InputStream is = MybatisProcess.class.getClassLoader().getResourceAsStream(resource);
         //构建sqlSession的工厂
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
         //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
@@ -59,7 +58,7 @@ public class Test1 {
     {
         System.out.println("开始！");
         String resource = "conf.xml";
-        InputStream is = Test1.class.getClassLoader().getResourceAsStream(resource);
+        InputStream is = MybatisProcess.class.getClassLoader().getResourceAsStream(resource);
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
         SqlSession session = sessionFactory.openSession();
         // 
@@ -73,8 +72,22 @@ public class Test1 {
         System.out.println("结束！");
     }
 
-    private void testE3()
+    private void testDualUser()
     {
+        System.out.println("开始DualUser！");
+        String resource = "conf.xml";
+        InputStream is = MybatisProcess.class.getClassLoader().getResourceAsStream(resource);
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession session = sessionFactory.openSession();
+        //
+        String statement = "com.dino.mybatisstudy.mapping.userMapper.getDualUser";//映射sql的标识字符串
+        List<User> userList = session.selectList(statement);
+        Iterator<User> it = userList.iterator();
+        while (it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+        System.out.println("结束！");
 
     }
 }
